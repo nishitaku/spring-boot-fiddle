ac6f8c78716482ac6bd5e19aea2a381407b104cd
nishitaku <29690066+nishitaku@users.noreply.github.com>
2023-05-19 14:26:52 +0900

    add test

--- src/test/java/com/example/demo/UnitDemoApplicationTests.java
+++ src/test/java/com/example/demo/UnitDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,6 +15,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
 // @SpringBootTest
 // @AutoConfigureMockMvc
 @WebMvcTest
@@ -25,9 +35,46 @@ class UnitDemoApplicationTests {
 	private DemoApplicationService demoApplicationService;
 
 	@Test
+	@DisplayName("GET /のテスト")
 	void helloWorld() throws Exception  {
-		mockMvc.perform(MockMvcRequestBuilders.get("/"))
-		.andExpect(MockMvcResultMatchers.status().isOk());
+		mockMvc.perform(get("/"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("index"))
+		.andExpect(content().string(containsString("Hello World")))
+		.andExpect(content().contentType("text/html;charset=UTF-8"))
+		.andExpect(xpath("/html/body/p").string("Hello World"))
+		.andExpect(xpath("//p").string("Hello World"));
+	}
+
+	@Test
+	@DisplayName("GET /formのテスト")
+	void getForm() throws Exception {
+		mockMvc.perform(get("/form"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("form"))
+		.andExpect(content().contentType("text/html;charset=UTF-8"))
+		.andExpect(xpath("//label[@for='name']").exists());
+	}
+
+	@Nested
+	@DisplayName("POST /formのテスト")
+	class PostFormTest {
+
+		@Test
+		@DisplayName("パターン1")
+		void postForm1() throws Exception {
+			mockMvc.perform(
+				post("/form")
+				.param("name", "taro")
+				.param("email", "taro@gmail.com")
+			)
+			.andExpect(status().isOk())
+			.andExpect(view().name("confirm"))
+			.andExpect(content().contentType("text/html;charset=UTF-8"))
+			.andExpect(xpath("/html/body/span").string(containsString("氏名： taro")))
+			.andExpect(xpath("/html/body/span").string(containsString("E-Mail： taro@gmail.com")))
+			.andExpect(xpath("/html/body/span").string(containsString("年齢： 100")));
+		}
 	}
 
 }

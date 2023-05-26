ff6904060779d644c8a3ad75421d19e6e9abeedf
nishitaku <29690066+nishitaku@users.noreply.github.com>
2023-05-26 10:19:15 +0900

    add dbunit test case

---
+++ src/test/java/com/example/demo/DemoApplicationIntegrationTests.java
@@ -0,0 +1,89 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dataset.CsvDataSetLoader;
+import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+@DbUnitConfiguration(dataSetLoader = CsvDataSetLoader.class)
+@SpringBootTest
+@Transactional
+@TestExecutionListeners({
+	DependencyInjectionTestExecutionListener.class,
+	TransactionDbUnitTestExecutionListener.class
+})
+@ActiveProfiles("devv")
+@AutoConfigureMockMvc
+public class DemoApplicationIntegrationTests {
+
+  @Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	@DisplayName("GET /のテスト")
+	void helloWorld() throws Exception  {
+		mockMvc.perform(get("/"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("index"))
+		.andExpect(content().string(containsString("Hello World")))
+		.andExpect(content().contentType("text/html;charset=UTF-8"))
+		.andExpect(xpath("/html/body/p").string("Hello World"))
+		.andExpect(xpath("//p").string("Hello World"));
+	}
+
+  @Nested
+	@DisplayName("GET /apiのテスト")
+	class ApiTest {
+
+		@Test
+		@DisplayName("パターン1")
+		void getApi1() throws Exception {
+			mockMvc.perform(
+				get("/api")
+				.param("name", "taro")
+				.param("email", "taro@gmail.com")
+				.param("age", "10")
+			)
+			.andExpect(status().isOk())
+			.andExpect(view().name("form"))
+			.andExpect(content().contentType("text/html;charset=UTF-8"))
+			.andExpect(xpath("//input[@type='text']/@value").string("taro"))
+			.andExpect(xpath("//input[@type='email']/@value").string("taro@gmail.com"))
+			.andExpect(xpath("//input[@type='number']/@value").string("10"))
+			.andExpect(xpath("/html/body/p").string("dog"));
+		}
+	}
+
+	@Test
+	@DisplayName("GET user")
+	@DatabaseSetup("classpath:dbunit/")
+	void getUser() throws Exception {
+		mockMvc.perform(
+			get("/user")
+			.param("userId", "2")
+		)
+		.andExpect(status().isOk())
+		.andExpect(view().name("user"))
+		.andExpect(content().contentType("text/html;charset=UTF-8"))
+		.andExpect(xpath("/html/body/span").string("jiro"));
+	}
+}
---
+++ src/test/java/com/example/demo/TestConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseDataSourceConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.ext.mysql.MySqlDataTypeFactory;
+import org.dbunit.ext.mysql.MySqlMetadataHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class TestConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public IDatabaseConnection dbUnitDatabaseConnection() throws SQLException {
+        // DatabaseDataSourceConnectionを使用する。テーブル名曖昧問題を解決するにはここでスキーマ名を設定する必要がある
+        // afterTestMethodでconnectionのcloseがされるのでDataSourceベースでないクラスを設定すると
+        // 2つ目以降のテストでconnectionがclose済みとなり失敗する
+        IDatabaseConnection conn = new DatabaseDataSourceConnection(dataSource, "serio");
+        // MetadataHandlerをMySqlMetadataHandlerに設定する
+        // この設定をしないとSchema修飾が正しく処理されずスキーマ間で同じ名前のテーブルがあるDBを扱うと失敗する
+        conn.getConfig().setProperty(DatabaseConfig.PROPERTY_METADATA_HANDLER, new MySqlMetadataHandler());
+        conn.getConfig().setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new MySqlDataTypeFactory());
+        return conn;
+    }
+}
